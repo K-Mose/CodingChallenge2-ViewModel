989ea8b19bd74b3c702ab55239264a0464123b9d
user <km_413@naver.com>
2021-07-10 17:41:23 +0900

    Changing Int to LiveData

--- app/src/main/java/com/example/codingchallenge2_viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge2_viewmodel/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.codingchallenge2_viewmodel.databinding.ActivityMainBinding
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -18,14 +19,25 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(111)
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
+
+        // encapsulate LiveData.
+        /*
+        LiveData는 Observer로 관찰하여 데이터에 변화가 오면
+        자동으로 UI를 업데이트 한다.
+        연관된 lifecycle에 변화가 생기면 LiveData가 인지하여서
+        destory될 때 스스로 clean up 하여 activity나 fragment가 종료될 때
+        메모리 누수나 크래쉬가 없다.
+         */
+        viewModel.totalData.observe(this, Observer { ld ->
+            binding.tvSum.text = ld.toString()
+        })
         binding.apply {
-            tvSum.text = viewModel.getCurrentNumber().toString()
             btnAdd.setOnClickListener {
                 etNumber.text.also {
                     if (it.isNullOrEmpty()) {
                         Toast.makeText(this@MainActivity, "Please put some number and try Again.", Toast.LENGTH_SHORT).show()
                     } else {
-                        tvSum.text = viewModel.addNumber(it.toString().toInt()).toString()
+                        viewModel.addNumber(it.toString().toInt())
                     }
                 }
             }
--- app/src/main/java/com/example/codingchallenge2_viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/codingchallenge2_viewmodel/MainActivityViewModel.kt
@@ -1,19 +1,24 @@
 package com.example.codingchallenge2_viewmodel
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
-    private var total = 0
-    init {
-            total = startingTotal
-    }
+    /*
+    LiveData vs MutableLiveData
+    Data in a LiveData object are Only readable, not editable
+    A MutableLiveData object allows us to change/update its data
+     */
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int> //
+    get() = total
 
-    fun getCurrentNumber(): Int = total
+    init {
+            total.value = startingTotal
+    }
     fun addNumber(number: Int): Int {
-        Log.d("getNumber ::","$number")
-        this.total += number
-        Log.d("thisNumber ::","${this.total}")
-        return this.total
+        total.value = (total.value)?.plus(number)
+        return total.value!!
     }
 }
